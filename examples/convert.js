1eaf42e1288ba00a850872e8804393f993039168
Guillaume Grossetie <ggrossetie@gmail.com>
2019-03-15 11:01:23 +0100

    Upgrade Asciidoctor.js 1.5.9 and enable linter

--- examples/convert.js
+++ examples/convert.js
@@ -2,4 +2,4 @@ const asciidoctor = require('asciidoctor.js')()
 const caniuseExtension = require('../src/asciidoctor-caniuse.js')
 const registry = caniuseExtension.register(asciidoctor.Extensions.create())
 
-asciidoctor.convertFile('sample.adoc', {safe: 'safe', header_footer: true, extension_registry: registry})
+asciidoctor.convertFile('sample.adoc', { safe: 'safe', header_footer: true, extension_registry: registry })
--- src/asciidoctor-caniuse.js
+++ src/asciidoctor-caniuse.js
@@ -21,38 +21,38 @@ const browserNames = {
   opera: 'Opera'
 }
 
-function caniuseInlineMacro() {
+function caniuseInlineMacro () {
   this.named('caniuse')
   this.positionalAttributes(['scope'])
   this.process((parent, target, attrs) => {
-    const scope = Opal.hash_get(attrs, 'scope') || 'last 1 Chrome versions,last 1 Firefox versions,last 1 Edge versions,last 1 Safari versions,last 1 Opera versions'
+    const scope = attrs.scope || 'last 1 Chrome versions,last 1 Firefox versions,last 1 Edge versions,last 1 Safari versions,last 1 Opera versions'
     caniuse.setBrowserScope(scope)
     const support = caniuse.getSupport(target)
     const latestStableBrowsers = caniuse.getLatestStableBrowsers()
       .map((value) => {
         const parts = value.split(' ')
-        return {id: parts[0], version: parts[1]}
+        return { id: parts[0], version: parts[1] }
       })
       .reduce((json, value) => {
         json[value.id] = value.version
         return json
       }, {})
     const content = Object.keys(support).map(function (browserId) {
-      let browserSupport = support[browserId];
-      let browserName = browserNames[browserId] || browserId;
-      let latestStableVersion = latestStableBrowsers[browserId];
-      const infos = [];
+      let browserSupport = support[browserId]
+      let browserName = browserNames[browserId] || browserId
+      let latestStableVersion = latestStableBrowsers[browserId]
+      const infos = []
       if (browserSupport.n) {
-        infos.push({classes: 'browser-version feat-unsupported', value: browserSupport.n})
+        infos.push({ classes: 'browser-version feat-unsupported', value: browserSupport.n })
       }
       if (browserSupport.x) {
-        infos.push({classes: 'browser-version feat-prefixed', value: browserSupport.x})
+        infos.push({ classes: 'browser-version feat-prefixed', value: browserSupport.x })
       }
       if (browserSupport.a) {
-        infos.push({classes: 'browser-version feat-partially-supported', value: browserSupport.a})
+        infos.push({ classes: 'browser-version feat-partially-supported', value: browserSupport.a })
       }
       if (browserSupport.y) {
-        infos.push({classes: 'browser-version feat-supported', value: browserSupport.y})
+        infos.push({ classes: 'browser-version feat-supported', value: browserSupport.y })
       }
       if (latestStableVersion) {
         let support = 'feat-unsupported'
@@ -62,7 +62,7 @@ function caniuseInlineMacro() {
           // supported on the latest stable version
           support = 'feat-supported'
         }
-        infos.push({classes: `browser-latest-stable-version ${support}`, value: latestStableVersion})
+        infos.push({ classes: `browser-latest-stable-version ${support}`, value: latestStableVersion })
       }
       return `<div class="browser">
   <div class="browser-name browser-${browserId}"><span>${browserName}</span></div>
@@ -75,7 +75,7 @@ function caniuseInlineMacro() {
   })
 }
 
-module.exports.register = function register(registry) {
+module.exports.register = function register (registry) {
   if (typeof registry.register === 'function') {
     registry.register(function () {
       this.inlineMacro(caniuseInlineMacro)
